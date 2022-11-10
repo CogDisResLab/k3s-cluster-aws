0c0605644e452e6b4e6d7545c0389dac38fb2ffe
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-11-09 22:47:10 -0500

    fix: add the account id to stop frivolous drift

--- dns.tf
+++ dns.tf
@@ -1,27 +1,31 @@
 resource "cloudflare_zone" "cdrlprojects_org" {
-  paused = false
-  plan   = "free"
-  type   = "full"
-  zone   = "cdrlprojects.org"
+  account_id = "8f6419333a38e403bba0e92e5dd92ecd"
+  paused     = false
+  plan       = "free"
+  type       = "full"
+  zone       = "cdrlprojects.org"
 }
 
 resource "cloudflare_zone" "cdrl_ut_org" {
-  paused = false
-  plan   = "free"
-  type   = "full"
-  zone   = "cdrl-ut.org"
+  account_id = "8f6419333a38e403bba0e92e5dd92ecd"
+  paused     = false
+  plan       = "free"
+  type       = "full"
+  zone       = "cdrl-ut.org"
 }
 
 resource "cloudflare_zone" "pic_ut_org" {
-  paused = false
-  plan   = "free"
-  type   = "full"
-  zone   = "pic-ut.org"
+  account_id = "8f6419333a38e403bba0e92e5dd92ecd"
+  paused     = false
+  plan       = "free"
+  type       = "full"
+  zone       = "pic-ut.org"
 }
 
 resource "cloudflare_zone" "precisioninformaticscore_org" {
-  paused = false
-  plan   = "free"
-  type   = "full"
-  zone   = "precisioninformaticscore.org"
+  account_id = "8f6419333a38e403bba0e92e5dd92ecd"
+  paused     = false
+  plan       = "free"
+  type       = "full"
+  zone       = "precisioninformaticscore.org"
 }
