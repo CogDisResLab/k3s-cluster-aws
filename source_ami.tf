c8720f2d80d303c52cca83779c11f9b8aee9a4a2
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-11-13 17:43:06 -0500

    feat: add ubuntu image

--- source_ami.tf
+++ source_ami.tf
@@ -7,3 +7,13 @@ data "aws_ami" "amazon_linux2" {
     values = ["amzn2-ami-hvm-*-arm64*"]
   }
 }
+
+data "aws_ami" "ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
+}
